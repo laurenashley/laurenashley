da59e92684a461da0ef1affcce7e443844395d53
Lauren Ashley <la.zaubi@gmail.com>
2024-05-21 15:46:37 -0700

    refactor code to format gists list

--- update_readme.py
+++ update_readme.py
@@ -1,16 +1,31 @@
 import json
 
 with open('gists.json') as f:
-    gists = json.load(f)
+  gists = json.load(f)
+
+filtered_gists = []
+
+for gist in gists:
+  filtered_gists.append(gist)
+
+# Format gists into a list
+gist_md = '<ul>\n'
+for gist in filtered_gists:
+  for filename in gist['files'].items():
+    gist_md += f"  <li><a href=\"{gist['html_url']}\">{filename}</a>\n"
+    gist_md += f"    <p>{gist['description']}</p>\n"
+    gist_md += f"  </li>\n"
+gist_md += '</ul>'
 
 gists_url = '\n'.join([f"- [{gist['description']}]({gist['url']})" for gist in gists])
 
 with open('README.md', 'r') as f:
-    readme = f.read()
+  readme = f.read()
 
+# Use partition method to insert gists into readme file
 marker = '<!-- gists -->'
 before, marker, after = readme.partition(marker)
-new_readme = f"{before}{marker}\n{gists_url}\n{after}"
+new_readme_content = f"{before}{marker}\n{gist_md}\n{after}"
 
 with open('README.md', 'w') as f:
-    f.write(new_readme)
+  f.write(new_readme_content)
