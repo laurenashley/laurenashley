5bc8f845a178848384a838a5b0fbeb96a7fb64f9
Lauren Ashley <la.zaubi@gmail.com>
2024-05-21 15:55:07 -0700

    limit gists list to 6 gists

--- update_readme.py
+++ update_readme.py
@@ -10,7 +10,7 @@ for gist in gists:
 
 # Format gists into a list
 gist_md = '<ul>\n'
-for gist in filtered_gists:
+for gist in filtered_gists[:6]:
   for filename in gist['files'].items():
     gist_md += f"  <li><a href=\"{gist['html_url']}\">{filename[0]}</a>\n"
     gist_md += f"    <p>{gist['description']}</p>\n"
