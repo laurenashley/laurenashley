b598ee7bab1118c10b6092e17079e083e3b4fd9a
Lauren Ashley <la.zaubi@gmail.com>
2024-05-21 16:00:20 -0700

    display most recent gists

--- update_readme.py
+++ update_readme.py
@@ -3,16 +3,17 @@ import json
 with open('gists.json') as f:
   gists = json.load(f)
 
-filtered_gists = []
+sorted_gists = sorted(gists, key=lambda x: x['created_at'], reverse=True)
+filtered_gists = sorted_gists[:6]
 
 for gist in gists:
   filtered_gists.append(gist)
 
 # Format gists into a list
 gist_md = '<ul>\n'
-for gist in filtered_gists[:6]:
-  for filename in gist['files'].items():
-    gist_md += f"  <li><a href=\"{gist['html_url']}\">{filename[0]}</a>\n"
+for gist in filtered_gists:
+  for filename, fileinfo in gist['files'].items():
+    gist_md += f"  <li><a href=\"{gist['html_url']}\">{filename}</a>\n"
     gist_md += f"    <p>{gist['description']}</p>\n"
     gist_md += f"  </li>\n"
 gist_md += '</ul>'
